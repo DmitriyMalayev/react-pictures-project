7e07edb81847ef9d4a8c8ff1c72a3659d761707e
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-08-30 01:43:35 -0400

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" name="" id="" /> 
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
+          </div>
         </form>
       </div>
     );
@@ -13,3 +27,20 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+/*
+ <input type="text" onChange={this.onInputChange} />;
+ this.onInputChange  does not have () because we don't want it invoked every time a render occurs but invoked only when there is an input.
+
+Controlled Element 
+  User types in input
+  Callback gets invoked
+  We call setState with a new value
+  Component Rerenders
+  Input is told what's it's value is (coming from state)
+this
+  Reference to the class itself 
+  Look to the left of the dot  
+
+
+ */
