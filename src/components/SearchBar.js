2e83828979e70b6fcb9b0ff40dc1f183a33ec695
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-08-29 23:11:35 -0400

    init commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" name="" id="" /> 
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
