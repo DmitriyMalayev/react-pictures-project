8faa74b5fb18b1ea3f51a61f50fa85501bd7352c
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-08-30 22:55:14 -0400

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,40 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term)
-    
-  }
+  state = { images: [] };
 
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+     
+    });
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+onSearchSubmit(term) {
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`,
+        },
+      })
+      .then((response) => console.log(response.data.results));
+  }
+
+  */
