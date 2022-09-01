cc2c702c09f2f67a2c770595d40d20918573cdd3
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-09-01 00:07:10 -0400

    ImageList

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 const ImageList = (props) => {
-  return <div>{props.images}</div>;
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} src={urls.regular} alt={description} />;
+  });
+  return <div>{images}</div>;
 };
 
 export default ImageList;
 
+// import React from "react";
 
+// const users = [
+//   { id: 1, name: "Leanne Graham" },
+//   { id: 2, name: "Ervin Howell" },
+//   { id: 3, name: "Clementine Bauch" },
+//   { id: 4, name: "Patricia Lebsack" },
+// ];
 
+// export default class App extends React.Component {
+//   render() {
+//     const listOfUsers = users.map((user) => {
+//       return <li key={user.id}>{user.name}</li>;
+//     });
+//     return <ul>{listOfUsers}</ul>;
+//   }
+// }
